325ef62b4c6997759953b4257bf99b4d591ccc09
Filipe Almeida <filipe.almeida@present-technologies.com>
2025-07-10 16:59:36 +0100

    Add RTK

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
+import { ReactNode } from 'react';
 import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import StoreProvider from '@/app/providers/StoreProvider';
+import '@/app/globals.css';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -20,11 +22,13 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <StoreProvider>{children}</StoreProvider>
+      </body>
     </html>
   );
 }
---
+++ src/lib/hooks.ts
@@ -0,0 +1,6 @@
+import { UseDispatch, useDispatch, UseSelector, useSelector, UseStore, useStore } from 'react-redux';
+import type { RootState, AppDispatch, AppStore } from '@/lib/store';
+
+export const useAppDispatch: UseDispatch<AppDispatch> = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector: UseSelector<RootState> = useSelector.withTypes<RootState>();
+export const useAppStore: UseStore<AppStore> = useStore.withTypes<AppStore>();
